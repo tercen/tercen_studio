4a24017468732657e092a6325a38b971e8d488f0
amaurel <alexandre.maurel@gmail.com>
2019-04-08 18:57:31 +0200

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# tercen studio
+
+Inside a terminal run the following
+
+```bash
+git clone 
+cd tercen_studio
+
+docker container run -d --name tercen_studio \
+ -p 8787:8787 \
+ -e PASSWORD=tercen \
+ -v ${PWD}/rstudio:/home/rstudio \
+ -w /home/rstudio \
+ tercen/tercen_studio:0.8.4
+
+```
+
+Open chrome [http://localhost:8787/](http://localhost:8787/)
+
+
+# Build
+
+```bash
+
+docker build -t tercen/tercen_studio:0.8.4 .
+docker push tercen/tercen_studio:0.8.4
+
+
+```
---
+++ rstudio/tercen_examples/upload_data.R
@@ -0,0 +1,50 @@
+library(tercen)
+
+serviceUri="https://dev.tercen.com/api/v1/"
+username="username"
+password="password"
+
+filename = '~/tercen_examples/crabs-long.csv'
+teamName = 'myteam'
+projectName = 'myproject'
+
+client = TercenClient$new(serviceUri=serviceUri,
+    username=username,
+    password=password)
+
+projects = client$documentService$findProjectByOwnersAndCreatedDate(
+    startKey=list(teamName,'2020'),
+    endKey=list(teamName,''))
+
+project = Find(function(p) identical(p$name,projectName), projects)
+project
+
+bytes = readBin(file(filename, 'rb'),raw(),n=file.info(filename)$size)
+
+fileDoc = FileDocument$new()
+fileDoc$name = 'crabs data'
+fileDoc$projectId = project$id
+fileDoc$acl$owner = project$acl$owner
+fileDoc$metadata = CSVFileMetadata$new()
+fileDoc$metadata$contentType = 'text/csv'
+fileDoc$metadata$separator = ','
+fileDoc$metadata$quote = '"'
+fileDoc$metadata$contentEncoding = 'iso-8859-1'
+fileDoc
+
+fileDoc = client$fileService$upload(fileDoc, bytes)
+
+task = CSVTask$new()
+task$state = InitState$new()
+task$fileDocumentId = fileDoc$id
+task$owner = project$acl$owner
+task$projectId = project$id
+
+task = client$taskService$create(task)
+client$taskService$runTask(task$id)
+task = client$taskService$waitDone(task$id)
+task
+if (inherits(task$state, 'FailedState')){
+    stop(task$state$reason)
+}
+# client$taskService$get(task$id)
