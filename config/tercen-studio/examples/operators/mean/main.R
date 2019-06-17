9108eaa26a2eb72fc740d4c4aadeb8ba82509306
amaurel <alexandre.maurel@gmail.com>
2019-06-17 14:16:35 +0200

    tercen server 0.3.16-server

--- config/tercen-studio/examples/operators/mean/main.R
+++ config/tercen-studio/examples/operators/mean/main.R
@@ -1,5 +1,10 @@
 library(tercen)
 library(dplyr)
+
+# http://127.0.0.1:5402/#ds/2ecef2b0b686d7fde25f34eeb8005605/3-1
+# options("tercen.workflowId"= "2ecef2b0b686d7fde25f34eeb8005605")
+# options("tercen.stepId"= "3-1")
+# getOption("tercen.serviceUri")
  
 (ctx = tercenCtx())  %>%
     select(.y, .ci, .ri) %>%
