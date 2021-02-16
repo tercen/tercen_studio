849243c43281a53bb62ef3304afaada6824eaf05
Alexandre Maurel <alexandre.maurel@gmail.com>
2021-02-16 12:01:50 +0100

    add missing package

--- config/tercen-studio/examples/api/data/team-storage.R
+++ config/tercen-studio/examples/api/data/team-storage.R
@@ -1,4 +1,5 @@
 library(tercen)
+library(tibble)
 
 client = TercenClient$new()
 client$session
@@ -7,7 +8,7 @@ client$session$user$id
 client$userService$profiles(client$session$user$id)
 client$userService$summary(client$session$user$id)
 client$userService$resourceSummary(client$session$user$id)
- 
+
 teamName = 'test-team'
 # usedStorage and usedCpuTime
 client$teamService$resourceSummary('test-team')
@@ -23,34 +24,36 @@ tibble(team = sapply(projects, function(each) each$acl$owner),
        usedCpuTime = sapply(projects, function(each) client$projectService$resourceSummary(each$id)$usedCpuTime),
        usedStorage = sapply(projects, function(each) client$projectService$resourceSummary(each$id)$usedStorage))
 
- 
+
 # all teams
 # need to be admin
 storage.summary = function(){
-  teams = client$teamService$findTeamByNameByLastModifiedDate(
-    startKey=list('ZZZZZZZZ','2030'),
-    endKey=list('',''))
-  
+  # teams = client$teamService$findTeamByNameByLastModifiedDate(
+  #   startKey=list('ZZZZZZZZ','2030'),
+  #   endKey=list('',''))
+
+  teams =  client$persistentService$findByKind('Team', useFactory=T)
+
   list = lapply(teams, function(team){
-    
+
     projects = client$documentService$findProjectByOwnersAndCreatedDate(
-      startKey=list(team$id,'2020'),
+      startKey=list(team$id,'2028'),
       endKey=list(team$id,''))
-    
+
     t = tibble(owner = rep(team$acl$owner, length(projects)),
                team = sapply(projects, function(each) each$acl$owner),
                project = sapply(projects, function(each) each$name),
                usedCpuTime = sapply(projects, function(each) client$projectService$resourceSummary(each$id)$usedCpuTime),
                usedStorage = sapply(projects, function(each) client$projectService$resourceSummary(each$id)$usedStorage))
-    
+
     return(t)
   })
-  
+
   return (do.call('rbind', list))
 }
 
-storage.summary()
- 
+strorage = storage.summary()
+
 # all users
 # need to be admin
 client$persistentService$findByKind('User', useFactory=T)
@@ -62,3 +65,11 @@ client$persistentService$findByKind('ComputedTableSchema', useFactory=F)
 client$persistentService$findByKind('CubeQueryTask', useFactory=F)
 client$persistentService$findByKind('ComputationTask', useFactory=F)
 client$persistentService$findByKind('CSVTask', useFactory=F)
+
+
+users = client$persistentService$findByKind('User', useFactory=T)
+
+sapply(users, function(user) user$email)
+sapply(users, function(user) user$isValidated)
+sapply(users, function(user) user$createdDate$value)
+
